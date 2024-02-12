3b11ad1bbf3e275f5f29333c2e9bf06c66c4626c
iasifahm02 <99195166+iasifahm02@users.noreply.github.com>
2024-02-12 18:11:21 +0530

    Time: 26 ms (30.89%) | Memory: 27.5 MB (17.32%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,10 +1,16 @@
 class Solution {
+private:
+    void reverse(vector<int>& nums, int start, int end){
+        while(start < end){
+            swap(nums[start++], nums[end--]);
+        }
+    }
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
 
-        // //Approach 1 - Brute force, Tc = O(n+k) | Sc = O(k)
+        //Approach 1 - Brute force, Tc = O(n+k) | Sc = O(k)
         // vector<int> temp;
         // for(int i = 0; i < n-k; i++){
         //     temp.push_back(nums[i]);
@@ -17,12 +23,17 @@ public:
         // }
 
         //Approach 2 - Better approach, Time and space both O(n)
-        vector<int> temp(n);
-        for(int i = 0; i < n; i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        for(int i = 0; i < n; i++){
-            nums[i] = temp[i];
-        }
+        // vector<int> temp(n);
+        // for(int i = 0; i < n; i++){
+        //     temp[(i+k)%n] = nums[i];
+        // }
+        // for(int i = 0; i < n; i++){
+        //     nums[i] = temp[i];
+        // }
+
+        //Approach 3: Optimise Approach - O(n) & O(1)
+        reverse(nums, 0, n-1); //Reverse whole array
+        reverse(nums, 0, k-1); //Reverse first k elements
+        reverse(nums, k, n-1); //Reverse n-k elements
     }
 };
