ad72689de3f7c301f8c9542688608a5a8a23cd79
iasifahm02 <99195166+iasifahm02@users.noreply.github.com>
2024-02-12 18:05:24 +0530

    Time: 26 ms (30.89%) | Memory: 28.1 MB (6.09%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,17 +4,25 @@ public:
         int n = nums.size();
         k = k%n;
 
-        vector<int> temp;
-        for(int i = 0; i < n-k; i++){
-            temp.push_back(nums[i]);
-        }
+        // //Approach 1 - Brute force, Tc = O(n+k) | Sc = O(k)
+        // vector<int> temp;
+        // for(int i = 0; i < n-k; i++){
+        //     temp.push_back(nums[i]);
+        // }
+        // for(int i = n-k; i < n; i++){
+        //     nums[i-(n-k)] = nums[i];
+        // }
+        // for(int i = k; i < n; i++){
+        //     nums[i] = temp[i-k];
+        // }
 
-        for(int i = n-k; i < n; i++){
-            nums[i-(n-k)] = nums[i];
+        //Approach 2 - Better approach, Time and space both O(n)
+        vector<int> temp(n);
+        for(int i = 0; i < n; i++){
+            temp[(i+k)%n] = nums[i];
         }
-
-        for(int i = k; i < n; i++){
-            nums[i] = temp[i-k];
+        for(int i = 0; i < n; i++){
+            nums[i] = temp[i];
         }
     }
 };
