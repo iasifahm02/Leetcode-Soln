6f3d41a3871d64bc25f1a310a5e3e12eb97a851d
iasifahm02 <99195166+iasifahm02@users.noreply.github.com>
2024-02-12 17:50:34 +0530

    Time: 27 ms (26.56%) | Memory: 27.8 MB (14.74%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,29 +1,20 @@
-#include<bits/stdc++.h>
 class Solution {
-private:
-    void reverse(vector<int> &arr, int start, int end){
-        while(start < end){
-            swap(arr[start++], arr[end--]);
-        }
-    }
 public:
     void rotate(vector<int>& nums, int k) {
-        // int n = nums.size();
-        // vector<int> temp(n);
-        // for(int i = 0; i < n; i++){
-        //     temp[(k+i)%n] = nums[i]; //Rotated towards right
-        //     // (n-k+i)%n > Rotated towards left
-        // }
-        // nums = temp;
-
-        //Optimal approach
         int n = nums.size();
         k = k%n;
-        //Reverse first k elements
-        reverse(nums, 0, n-k-1);
-        //Reverse another n-k elements
-        reverse(nums, n-k, n-1);
-        //Reverse whole array
-        reverse(nums, 0, n-1);
+
+        vector<int> temp;
+        for(int i = 0; i < n-k; i++){
+            temp.push_back(nums[i]);
+        }
+
+        for(int i = n-k; i < n; i++){
+            nums[i-(n-k)] = nums[i];
+        }
+
+        for(int i = k; i < n; i++){
+            nums[i] = temp[i-k];
+        }
     }
 };
