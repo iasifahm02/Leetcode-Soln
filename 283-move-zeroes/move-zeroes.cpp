d11fd28d8ad5637899061fccefcae398acbbbe27
iasifahm02 <99195166+iasifahm02@users.noreply.github.com>
2024-02-16 23:25:34 +0530

    Time: 13 ms (82.61%) | Memory: 21.5 MB (33.79%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,10 +4,11 @@ public:
         int j = 0;
         for(int i = 1; i < nums.size(); i++){
             if(nums[j] == 0 && nums[i] != 0){
-                swap(nums[j], nums[i]);
+                swap(nums[j++], nums[i]);
+            }
+            else if(nums[j] != 0){
                 j++;
             }
-            else if(nums[j] != 0) j++;
         }
     }
 };
