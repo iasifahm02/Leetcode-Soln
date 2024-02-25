395ec7db0895f0ed5a4f9952e4033b6a167302e1
iasifahm02 <99195166+iasifahm02@users.noreply.github.com>
2024-02-25 17:58:10 +0530

    Time: 3 ms (90.97%) | Memory: 16.1 MB (31.15%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,39 @@
 class Solution {
 private:
-    int firstPosition(vector<int> nums, int target){
-        int low = 0;
-        int high = nums.size()-1;
+    int firstPos(vector<int>& nums, int target){
+        int low = 0, high = nums.size()-1;
         int ans = -1;
-
         while(low <= high){
-            int mid = low + (high-low)/2;
-
+            int mid = low+ (high-low)/2;
             if(nums[mid] == target){
                 ans = mid;
                 high = mid-1;
             }
-            else if(target > nums[mid]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
+            else if(nums[mid] < target) low = mid+1;
+            else high = mid-1;
         }
         return ans;
     }
 
-    int lastPosition(vector<int> nums, int target){
-        int low = 0;
-        int high = nums.size()-1;
+    int lastPos(vector<int>& nums, int target){
+        int low = 0, high = nums.size()-1;
         int ans = -1;
-
         while(low <= high){
-            int mid = low + (high-low)/2;
 
+            int mid = low+ (high-low)/2;
             if(nums[mid] == target){
                 ans = mid;
                 low = mid+1;
             }
-            else if(target > nums[mid]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
+            else if(nums[mid] < target) low = mid+1;
+            else high = mid-1;
         }
         return ans;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        ans.push_back(firstPosition(nums, target));
-        ans.push_back(lastPosition(nums, target));
-        return ans;
+        int first = firstPos(nums, target);
+        int last = lastPos(nums, target);
+        return {first, last};
     }
 };
